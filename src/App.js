0ee36291788e8f24c61db3e2615ccb30c3ffb513
Shashant Shekhar <shashantshekhar10@gmail.com>
2023-07-09 00:58:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import UseReducer from "./components/useReducer";
+import UseState from "./components/useState";
+
+function App() {
+  return (
+    <>
+      {/* <UseState /> */}
+      <UseReducer />
+      
+    </>
+  );
+}
+
+export default App;
